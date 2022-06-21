ed8619f3eecbe5a17a7473706ea2ff79ef84fa05
Irwan Hermawan <rwnhermawan@gmail.com>
2022-06-21 15:13:14 +0700

    remove

--- MyJetpack (Clean-Arch) Starter/app/src/main/java/com/kodev/games/data/source/remote/response/ResponseGame.kt
+++
@@ -1,9 +0,0 @@
-package com.kodev.games.data.source.remote.response
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class ResponseGame(
-    val results: List<DataGame>
-): Parcelable
